49f6870754cbdc038e506e8f84c8aa43315e5aaf
Jafet19 <kacur99@gmail.com>
2022-08-31 16:01:09 -0500

    commit tree b77d7d375ef5c0f145666cea898bd6a52637af25

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+// function addEventListener(){
+//     let button = document.getElementById('button');
+//     button.addEventListener('click', (event) => alert('clicked'))
+// }
+
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', addingEventListener);
 }
